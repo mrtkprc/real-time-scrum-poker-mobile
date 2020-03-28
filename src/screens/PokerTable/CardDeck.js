c4463d9b5d4659ed9a956d7c7727d109f352123b
mrtkprc <123456ORKebru>
2020-03-28 23:09:24 +0300

    CardDeck is introduced.

---
+++ src/screens/PokerTable/CardDeck.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Card from "../../components/Card";
+import {StyleSheet, View} from "react-native";
+
+const CardDeck = (props) => {
+    const cards = [];
+    const cardPoints = ["0","0.5","1","2","3","5","8","13","20","40","100","Inf","Q", "C"];
+    const ROW_COUNT = 4;
+    const CARD_COUNT_EACH_ROW = 4;
+    let cardsInRow = [];
+    let currentCardIndex;
+    let currentCard;
+
+
+    for(let i = 0; i < ROW_COUNT ; i++){
+        cardsInRow = [];
+
+        for (let j = 0; j < CARD_COUNT_EACH_ROW; j++) {
+            currentCardIndex = i * CARD_COUNT_EACH_ROW + j;
+
+            if(currentCardIndex >= cardPoints.length)
+                break;
+            currentCard = cardPoints[currentCardIndex];
+            cardsInRow.push(
+                <Card
+                    isImage={currentCard === 'Q' || currentCard === 'C' || currentCard === 'Inf'}
+                    isCardSelected={props.selectedCard === currentCard}
+                    cardPressed={props.cardPressed}
+                    key={"SP"+currentCard}
+                    point={currentCard}/>
+            );
+        }
+        cards.push(<View key={"CardRow"+i} style={styles.cardShowingAreaBaseRow}>{cardsInRow}</View>)
+    }
+    return (
+        cards
+    );
+};
+
+
+const styles = StyleSheet.create({
+    cardShowingAreaBaseRow:{
+        alignItems: 'center',
+        justifyContent:'space-around',
+        flexDirection: 'row',
+        flex: 1,
+        backgroundColor: '#03d3fc'
+    }
+});
+
+export default CardDeck;
+
--- src/screens/PokerTable/PokerTable.js
+++ src/screens/PokerTable/PokerTable.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
-import Card from '../../components/Card';
 import ListItem from "./ListItem";
+import CardDeck from "./CardDeck";
 
 export default class PokerTable extends Component {
     state = {selectedCard: 'Q'};
@@ -12,46 +12,15 @@ export default class PokerTable extends Component {
         });
     };
 
-    composeCards() {
-        const cards = [];
-        const cardPoints = ["0","0.5","1","2","3","5","8","13","20","40","100","Inf","Q", "C"];
-        const ROW_COUNT = 4;
-        const CARD_COUNT_EACH_ROW = 4;
-        let cardsInRow = [];
-        let currentCardIndex;
-        let currentCard;
-        for(let i = 0; i < ROW_COUNT ; i++){
-            cardsInRow = [];
-
-            for (let j = 0; j < CARD_COUNT_EACH_ROW; j++) {
-                currentCardIndex = i * CARD_COUNT_EACH_ROW + j;
-
-                if(currentCardIndex >= cardPoints.length)
-                    break;
-                currentCard = cardPoints[currentCardIndex];
-                cardsInRow.push(
-                    <Card
-                        isImage={currentCard === 'Q' || currentCard === 'C' || currentCard === 'Inf'}
-                        isCardSelected={this.state.selectedCard === currentCard}
-                        cardPressed={this.cardPressed}
-                        key={"SP"+currentCard}
-                        point={currentCard}/>
-                );
-            }
-            cards.push(<View key={"CardRow"+i} style={styles.cardShowingAreaBaseRow}>{cardsInRow}</View>)
-        }
-        return cards;
-    };
-
     render() {
         const data = [{id: 1, username:"mrtkprc"},{id: 2, username:"emre"},{id: 3, username:"kaan"},{id: 4, username:"oytun"}];
         return (
             <View style={styles.container}>
                 <View style={styles.cardArea}>
                     <View style={styles.cardAreaShowingCards}>
-                        {
-                            this.composeCards()
-                        }
+                        <CardDeck
+                            cardPressed={this.cardPressed}
+                            selectedCard={this.state.selectedCard}/>
                     </View>
                     <View style={styles.cardAreaSelectedCardStatus}>
                         <Text>Selected Card Status</Text>
@@ -82,19 +51,12 @@ const styles = StyleSheet.create({
         backgroundColor: 'blue',
         flex: 1
     },
-    cardAreaShowingCards:{
-        flex:4,
-        flexDirection: 'column',
-    },
     cardAreaSelectedCardStatus:{
         flex: 1,
         backgroundColor: 'magenta'
     },
-    cardShowingAreaBaseRow:{
-        alignItems: 'center',
-        justifyContent:'space-around',
-        flexDirection: 'row',
-        flex: 1,
-        backgroundColor: '#03d3fc'
-    },
+    cardAreaShowingCards:{
+        flex:4,
+        flexDirection: 'column',
+    }
 });
