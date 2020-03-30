70b03a88bef0797aaa42499ea7575d8572707440
mrtkprc <123456ORKebru>
2020-03-30 08:13:42 +0300

    When New Participant is arrived, participant is added without refetch.

--- src/screens/PokerTable/ParticipantList.js
+++ src/screens/PokerTable/ParticipantList.js
@@ -1,44 +1,49 @@
-import React, {useState} from 'react';
-import {useQuery, useSubscription} from '@apollo/react-hooks';
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import {PARTICIPANTS_IN_SESSION_QUERY, NEW_PARTICIPANT_ARRIVED_SUBSCRIPTION} from './queries'
-import {ScrollView, Text, View, FlatList} from "react-native";
+import { ScrollView, Text, View, FlatList } from "react-native";
 import ListItem from "./ListItem";
 
 const ParticipantList = (props) => {
-    const { loading, error, data, refetch } = useQuery(PARTICIPANTS_IN_SESSION_QUERY,{
-        variables: { "id":  props.sessionId },
+    const {loading, error, data, subscribeToMore} = useQuery(PARTICIPANTS_IN_SESSION_QUERY, {
+        variables: {"id": props.sessionId},
     });
 
-    if (loading) return <View><Text>Loading...</Text></View>
-    if (error) return  <ScrollView><Text>{JSON.stringify(error)}</Text></ScrollView>
+    if (loading) return <View><Text>Loading...</Text></View>;
+    if (error) return <ScrollView><Text>{JSON.stringify(error)}</Text></ScrollView>;
+
+    subscribeToMore({
+        document: NEW_PARTICIPANT_ARRIVED_SUBSCRIPTION,
+        variables: {
+            sessionId: props.sessionId
+        },
+        updateQuery: (prev, {subscriptionData}) => {
+            if (!subscriptionData.data) return prev;
+            const newParticipant = subscriptionData.data.newParticipantArrived;
+            if (!prev.session.participants.find((participant) => participant.id === newParticipant.id)) {
+                return {
+                    ...prev,
+                    session: {
+                        ...prev.session,
+                        participants: [
+                            newParticipant,
+                            ...prev.session.participants
+                        ]
+                    }
+                };
+            } else {
+                return prev;
+            }
+        }
+    });
 
     return (
-        <>
-            <FlatList
-                data={data.session.participants}
-                renderItem={({item}) => <ListItem item={item}/> }
-                keyExtractor={item => item.id}
-            />
-            <NewParticipantArrived
-                sessionId={props.sessionId}
-                refetch={refetch}
-                participantList={data.session.participants}/>
-        </>
+        <FlatList
+            data={data.session.participants}
+            renderItem={({item}) => <ListItem item={item}/>}
+            keyExtractor={item => item.id}
+        />
     );
-};
-
-const NewParticipantArrived = (props) => {
-    const { data, loading } = useSubscription(
-        NEW_PARTICIPANT_ARRIVED_SUBSCRIPTION,
-        { variables: { "sessionId": props.sessionId } }
-    );
-
-    if(!loading)
-    {
-        props.refetch();
-    }
-
-    return null;
 };
 
 export default ParticipantList;
