dbee8d8c1c8b9b5a04eb824ed4cd19bfa5350413
mrtkprc <123456ORKebru>
2020-03-31 12:39:42 +0300

    Queries of AttendPoker is updated.

--- src/screens/AttendPoker/queries.js
+++ src/screens/AttendPoker/queries.js
@@ -1,11 +1,11 @@
 import {gql} from "apollo-boost";
 
-export const POKEMONS_QUERY = gql`
-    {
-        pokemons(first: 20) {
+export const FIND_SESSION_BY_NUMBER_QUERY = gql`
+    query findSessionBySessionNumber($sessionNumber: Int!) {
+        findSessionBySessionNumber(sessionNumber: $sessionNumber)
+        {
             id
-            name
-            image
+            sessionNumber
         }
     }
 `;
