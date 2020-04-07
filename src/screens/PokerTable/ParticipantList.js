46950069eeb8132510ab90d9defd4b9d86a0c115
mrtkprc <123456ORKebru>
2020-04-07 22:50:28 +0300

    Memory Leak Error about subscriptions is fixed.

--- src/screens/PokerTable/ParticipantList.js
+++ src/screens/PokerTable/ParticipantList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { PARTICIPANTS_IN_SESSION_QUERY, NEW_PARTICIPANT_ARRIVED_SUBSCRIPTION, VOTE_GIVEN_SUBSCRIPTION } from './queries'
 import { ScrollView, Text, View, FlatList} from "react-native";
@@ -8,72 +8,78 @@ import { useForceUpdate } from "../../helpers/general";
 
 const ParticipantList = (props) => {
     const [newParticipantId, setNewParticipantId] = useState("");
+    const [isSubscriptionAdjusted,setIsSubscriptionAdjusted] = useState(false);
     const {loading, error, data, subscribeToMore} = useQuery(PARTICIPANTS_IN_SESSION_QUERY, {
         variables: {"id": props.sessionId},
     });
+
     const forceUpdate = useForceUpdate();
 
     if (loading) return <View><Text>Loading...</Text></View>;
     if (error) return <ScrollView><Text>{JSON.stringify(error)}</Text></ScrollView>;
 
-    subscribeToMore({
-        document: NEW_PARTICIPANT_ARRIVED_SUBSCRIPTION,
-        variables: {
-            sessionId: props.sessionId
-        },
-        updateQuery: (prev, {subscriptionData}) => {
-            if (!subscriptionData.data) return prev;
-            const newParticipant = subscriptionData.data.newParticipantArrived;
-            if (!prev.session.participants.find((participant) => participant.id === newParticipant.id)) {
-                setNewParticipantId(newParticipant.id);
-                return {
-                    ...prev,
-                    session: {
-                        ...prev.session,
-                        participants: [
-                            newParticipant,
-                            ...prev.session.participants
-                        ]
-                    }
-                };
-            } else {
-                return prev;
+    if(!isSubscriptionAdjusted)
+    {
+        subscribeToMore({
+            document: NEW_PARTICIPANT_ARRIVED_SUBSCRIPTION,
+            variables: {
+                sessionId: props.sessionId
+            },
+            updateQuery: (prev, {subscriptionData}) => {
+                if (!subscriptionData.data) return prev;
+                const newParticipant = subscriptionData.data.newParticipantArrived;
+                if (!prev.session.participants.find((participant) => participant.id === newParticipant.id)) {
+                    setNewParticipantId(newParticipant.id);
+                    return {
+                        ...prev,
+                        session: {
+                            ...prev.session,
+                            participants: [
+                                newParticipant,
+                                ...prev.session.participants
+                            ]
+                        }
+                    };
+                } else {
+                    return prev;
+                }
             }
-        }
-    });
+        });
+        subscribeToMore({
+            document: VOTE_GIVEN_SUBSCRIPTION,
+            variables: {
+                sessionId: props.sessionId
+            },
+            updateQuery:  (prev, {subscriptionData}) => {
+                if (!subscriptionData.data) return prev;
+                const voteGiven = subscriptionData.data.voteGiven;
+                if(voteGiven) {
+                    prev.session.participants.forEach((participant, index) => {
+                        if(participant.id === voteGiven.participant.id) {
+                            const copyValue = Object.assign({},prev);
 
-    subscribeToMore({
-        document: VOTE_GIVEN_SUBSCRIPTION,
-        variables: {
-            sessionId: props.sessionId
-        },
-        updateQuery:  (prev, {subscriptionData}) => {
-            if (!subscriptionData.data) return prev;
-            const voteGiven = subscriptionData.data.voteGiven;
-            if(voteGiven) {
-                prev.session.participants.forEach((participant, index) => {
-                    if(participant.id === voteGiven.participant.id) {
-                        const copyValue = Object.assign({},prev);
+                            copyValue.session.participants[index] = {
+                                ...participant,
+                                vote: {
+                                    isGiven: 1
+                                }
+                            };
 
-                        copyValue.session.participants[index] = {
-                            ...participant,
-                            vote: {
-                                isGiven: 1
-                            }
-                        };
-
-                        forceUpdate();
-                        return copyValue;
-                    }
-                    else
-                        return prev;
-                })
+                            forceUpdate();
+                            return copyValue;
+                        }
+                        else
+                            return prev;
+                    })
+                }
+                else
+                    return prev;
             }
-            else
-                return prev;
-        }
-    });
+        });
 
+        setIsSubscriptionAdjusted(true);
+
+    }
     return (
         <FlatList
             data={data.session.participants}
