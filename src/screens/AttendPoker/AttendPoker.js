0ae4d12b6696cb99b0ba230b663f920568e97ca7
mrtkprc <123456ORKebru>
2020-04-13 12:37:49 +0300

    New UI of AttenPoker is started

--- src/screens/AttendPoker/AttendPoker.js
+++ src/screens/AttendPoker/AttendPoker.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {StyleSheet, TouchableOpacity, TextInput, View} from 'react-native';
-import {Container, Content, Item, Input, Thumbnail, Button, Text} from 'native-base';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {StyleSheet, Image, TouchableOpacity, Text, TextInput, View} from 'react-native';
 import {Formik} from 'formik';
 import {useMutation} from '@apollo/react-hooks';
 import {CREATE_PARTICIPANT_MUTATION} from './queries';
 import {useNavigation} from '@react-navigation/native';
 import Loading from "../../components/Loading";
-
+import Error from "../../components/Error";
+import LinearGradient from 'react-native-linear-gradient';
+import {Item, Input, Icon} from 'native-base';
 const AttendPoker = () => {
     const [addParticipant, {loading, error} ] = useMutation(CREATE_PARTICIPANT_MUTATION);
     const navigation = useNavigation();
@@ -16,7 +16,6 @@ const AttendPoker = () => {
     const formSubmitted = (values) => {
         addParticipant({variables:{nickname: values.fullName, sessionNumber: parseInt(values.sessionNumber)}})
             .then(addedData => {
-                console.log("Clicked CreateParticipant");
                 const {data: {createParticipant:{id, session}}} = addedData;
                 navigation.navigate('PokerTable',{
                     sessionId: session.id,
@@ -34,47 +33,19 @@ const AttendPoker = () => {
     };
 
     return (
-        <Formik
-            onSubmit={formSubmitted}
-            initialValues={{sessionNumber: '123456', fullName: 'ork'}}
-        >
+        <Formik onSubmit={formSubmitted} initialValues={{sessionNumber: '123456', fullName: 'ork'}}>
             {({handleChange, handleBlur, handleSubmit, values}) => (
                 <>
                 { loading && <Loading text="Loading Poker Table" />}
-                { error && <View><Text>Error</Text></View>}
-                <Container style={styles.container}>
-                    <Content>
-                        <Thumbnail style={styles.logo} large source={{uri: uri}}/>
-                        <Item style={styles.sessionNumberItem} rounded>
-                            <TextInput
-                                name={"sessionNumber"}
-                                value={values.sessionNumber}
-                                onChangeText={handleChange('sessionNumber')}
-                                maxLength={6}
-                                style={styles.sessionNumberInput}
-                                keyboardType='number-pad'
-                                placeholder='Session Number'/>
-                        </Item>
-                        <Item style={styles.fullNameItem} regular>
-                            <Icon style={{marginLeft: 10}} size={48} color='#06E399' active name='user'/>
-                            <Input
-                                name={"fullName"}
-                                value={values.fullName}
-                                onChangeText={handleChange('fullName')}
-                                style={styles.fullNameInput}
-                                placeholder='Full Name'/>
-                        </Item>
-                    </Content>
-                    <Content>
-                        <Button onPress={handleSubmit} style={styles.createSessionButton} iconLeft success full>
-                            <Icon type="FontAwesome" size={32} color='white' name='user'/>
-                            <Text style={styles.createSessionButtonText}>Create Session</Text>
-                        </Button>
-                    </Content>
-                    <TouchableOpacity onPress={startPokerPressed} style={styles.startAPokerButton}>
-                        <Text style={{color: 'yellow', fontSize: 18, fontWeight: 'bold'}}>Start A Poker</Text>
-                    </TouchableOpacity>
-                </Container>
+                { error && <Error/>}
+                <LinearGradient colors={['#30cfd0', '#330867']} style={styles.container}>
+                    <Text>Real Time Scrum Poker</Text>
+                    <Image source={require('./../../assets/logo.png')} style={styles.logo} />
+                    <View style={styles.sessionNumberInputArea}>
+                        <TextInput style={styles.sessionNumberInput} placeholder='Session Number'/>
+                    </View>
+
+                </LinearGradient>
                 </>
             )}
         </Formik>
@@ -82,64 +53,26 @@ const AttendPoker = () => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        flexDirection: 'column'
-    },
-    header: {
-        backgroundColor: "#06BEE3",
-    },
-    headerTitle: {
-        marginLeft: 'auto',
-        marginRight: 'auto',
-    },
-    logo: {
-        marginTop: 30,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-    },
-    sessionNumberItem: {
-        marginTop: 15,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        width: '60%'
-    },
-    sessionNumberInput: {
-        textAlign: 'center',
-        fontSize: 24,
-    },
-    fullNameItem: {
-        marginTop: 30,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        width: '90%',
-        borderColor: '#06BEE3',
-        borderWidth: 3
-    },
-    fullNameInput: {
-        textAlign: 'center',
-        fontSize: 24,
-        marginLeft: -40
-    },
-    createSessionButton: {
-        width: '80%',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        color: '#06BEE3',
-        marginBottom: 10
-    },
-    createSessionButtonText: {
-        fontSize: 22,
-    },
-    startAPokerButton: {
-        justifyContent: 'flex-end',
+    container:{
+        flex: 1,
+        justifyContent: 'flex-start',
         alignItems: 'center',
-        width: '100%',
-        backgroundColor: "#333",
-        height: 50,
-        textAlign: 'center',
-        paddingBottom: 10,
+    },
+    logo:{
+        marginVertical: 10,
+        width: 200,
+        height: 200
+    },
+    sessionNumberInputArea:{
+        width: '80%',
+    },
+    sessionNumberInput:{
+        opacity: 0.5,
+        borderRadius: 40,
+        paddingLeft: 10,
+        backgroundColor: 'white'
     }
+
 });
 
 export default AttendPoker;
